adc41ffee24375ca82d225d445d6d9e4b0b01e79
Jenwit <jenwitkmonz@gmail.com>
2018-08-20 14:21:28 +0700

    first commit

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,11 @@
+<?php 
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . 'third_party/MX/Controller.php';
+
+class MY_Controller extends MX_Controller{
+    public function __construct(){
+        parent::__construct();
+    }
+}
---
+++ application/modules/readme/views/readme.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Readme</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="jumbotron">
+        <h1 class="display-3">Please read</h1>
+        <p class="lead">Codeigniter with HMVC and MY_Controller</p>
+        <div class="container">
+            <h1 class="display-4">To config before use it</h1>
+            <ol>
+                <li>In config folder, a config.php file add your base url</li>
+                <li>In config folder, a route.php file change a default_controller to your default_controller</li>
+                <li>Now your working space is not in the controllers,models and views folder but inside a module</li>
+                <li>In modules you have to create a folder that contains controllers,models and views folder</li>
+                <li>About controller class you have to use <b>MY_Controller</b> instead of <b>CI_Controller</b></li>
+                <li>Have fun</li>
+            </ol>
+        </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+  </body>
+</html>
